22d9b362ad201dcae4239b53c69dbfc75d202384
Matt Tyndall <mattctyndall@gmail.com>
2017-01-15 13:05:54 -0600

    Start figuring out refund and payout

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -19,6 +19,30 @@ contract Project {
     uint public totalFunding;
     uint public numOfContributions;
 
+    event LogContributionReceived(address projectAddress, address contributor, uint amount);
+    event LogPayoutInitiated(address projectAddress, address owner, uint totalPayout);
+    event LogRefundIssued(address projectAddress, address contributor, uint refundAmount);
+    event LogFundingGoalReached(address projectAddress, uint totalFunding, uint totalContributions);
+    event LogFundingFailed(address projectAddress, uint totalFunding, uint totalContributions);
+
+    modifier onlyOwner {
+        if (owner != msg.sender) throw;
+        _;
+    }
+
+    modifier onlyFailedProject {
+        // Deadline should be over
+        if (block.number < deadline) {
+            throw;
+        }
+
+        // Funding goal should not have been reached
+        if (totalFunding >= fundingGoal) {
+            throw;
+        }
+        _;
+    }
+
     function Project(uint _fundingGoal, uint _deadline, string _title) {
         owner = msg.sender;
         fundingGoal = _fundingGoal;
@@ -26,7 +50,7 @@ contract Project {
         title = _title;
 
         totalFunding = 0;
-        numOfContributions = 0;
+        numOfContributions = 0; // TODO if contributions are mapped, what value does this provide?
     }
 
     /**
@@ -36,22 +60,80 @@ contract Project {
     * (Optionally could only allow contributions from the creating FundingHub).
     */
     function fund(uint _amount, address _contributor) payable returns (bool successful) {
+        // if funding reached, refund sender
+        if (totalFunding >= fundingGoal) {
+            refund()
+            return false;
+        }
+
+        // if deadline reached, refund sender
+        if (block.number > deadline) {
+            refund();
+            return false;
+        }
+
         Contribution c = contributions[_contributor]; // Should contributions be mapped by address or index 
         c.contributor = _contributor;
-        c.amount = _amount;
+        c.amount += _amount; // It's possible that a user contributes more than once
 
         totalFunding += _amount;
         numOfContributions++;
 
+        LogContributionReceived(this, _contributor, _amount);
+
+        // check success criteria
+        if (totalFunding >= fundingGoal) {
+            payout();
+        }
+
         return true;
     }
 
+    /**
+    * If funding goal has been met, transfer fund to project owner
+    */
     function payout() payable returns (bool successful) {
+        if (totalFunding < fundingGoal) {
+            throw;
+        }
+
+        uint amount = totalFunding;
+
+        // prevent re-entrancy
+        totalFunding = 0;
+
+        if (owner.send(amount)) {
+            return true;
+        } else {
+            totalFunding = amount;
+            return false;
+        }
+
         return true;
     }
 
-    function refund() payable returns (bool successful) {
+    /**
+    * If the deadline is passed and the goal was not reached, allow contributors to withdraw their contributions
+    */
+    function refund() payable onlyFailedProject returns (bool successful) {
+        Contribution c = contributions[msg.sender];
+        uint amount = c.amount;
+
+        // Remember to zero the pending refund before
+        // sending to prevent re-entrancy attack
+        c.amount = 0;
+
+        if (msg.sender.send(amount)) {
+            return true;
+        } else {
+            c.amount = amount;
+            return false;
+        }
         return true;
+    }
+
+    function kill() public onlyOwner {
+        selfdestruct(owner);
     }
 
     /** 
